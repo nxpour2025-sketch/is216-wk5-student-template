76b72df2391ed942eb0524b5eb4dff81321c75c1
nxpour2025-sketch <nx.pour.2025@smu.edu.sg>
2026-09-14 00:13:58 +0800

    ex2 try3

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -46,7 +46,7 @@ function reset() {
 
         <div style="margin-top: 14px;">
             <!-- C. Display winner / status here -->
-            <p>Winner: {{ scoreA === maxScore ? teamA : scoreB === maxScore ? teamB : "No winner yet. Keep playing!" }}</p>
+            <p>{{ scoreA === maxScore ? "Winner: "+ teamA : scoreB === maxScore ? "Winner: " + teamB : "No winner yet. Keep playing!" }}</p>
         </div>
 
 
