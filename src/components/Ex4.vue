2a978ec087529612dde920497284163de379a744
nxpour2025-sketch <nx.pour.2025@smu.edu.sg>
2026-09-14 01:42:57 +0800

    ex4

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -3,7 +3,14 @@ import { ref } from 'vue'
 const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
 
 // TODO: Add Code Here
+const add = ref('')
+function onDelete(index) {
+    return items.value.splice(index,1)
+}
 
+function onAdd(item) {
+    return items.value.push(item)
+}
 
 </script>
 
@@ -11,6 +18,13 @@ const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+    <ul>
+        <li v-for="(item, index) in items">
+            {{ item }}
+            <button @click="onDelete(index)" value="index">Delete!</button>
+        </li>
+    </ul>
 
+    <p><input v-model="add"><button @click="onAdd(add)">Add!</button></p>
    
 </template>
